24f2d9e6ae9ed0bf4bb4bdabfbf7d1c9ee8c6a45
dasda98 <wojniakjakub@gmail.com>
2024-07-17 11:10:49 +0200

    Add vector code

--- include/vector.h
+++ include/vector.h
@@ -1,7 +1,19 @@
 #ifndef VECTOR_IN_C_LIBRARY_H
 #define VECTOR_IN_C_LIBRARY_H
 
+#include <stddef.h>
 
-void hello(void);
+
+typedef struct Vector {
+    void* data;
+    size_t size;
+    size_t capacity;
+    size_t elem_size;
+} Vector;
+
+void vector_init(Vector* vec, size_t elem_size);
+void vector_push_back(Vector* vec, void* elem);
+void* vector_get(Vector* vec, size_t index);
+void vector_free(Vector* vec);
 
 #endif //VECTOR_IN_C_LIBRARY_H
--- src/vector.c
+++ src/vector.c
@@ -1,7 +1,36 @@
 #include <vector.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+void vector_init(Vector* vec, size_t elem_size) {
+    vec->data = NULL;
+    vec->size = 0;
+    vec->capacity = 0;
+    vec->elem_size = elem_size;
+}
+
+void vector_push_back(Vector* vec, void* elem) {
+    if (vec->size == vec->capacity) {
+        vec->capacity = vec->capacity == 0 ? 1 : vec->capacity * 2;
+        vec->data = realloc(vec->data, vec->capacity * vec->elem_size);
+    }
+
+    memcpy((char*)vec->data + vec->size * vec->elem_size, elem, vec->elem_size);
+    vec->size++;
+}
+
+void* vector_get(Vector* vec, size_t index) {
+    if (index < vec->size) {
+        return (char*)vec->data + index * vec->elem_size;
+    }
+    return NULL;
+}
+
+void vector_free(Vector* vec) {
+    free(vec->data);
+    vec->data = NULL;
+    vec->size = 0;
+    vec->capacity = 0;
 }
